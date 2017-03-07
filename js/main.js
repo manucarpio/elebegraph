ec3d324a21dc9eb84b0e35eae37fa7a431c9386e
Manuel <manuel.carpio@elebegraph.com>
2017-03-07 14:55:15 -0600

    aplicando diseño responsive

--- js/main.js
+++ js/main.js
@@ -38,3 +38,17 @@
 	});  
 	
 })(jQuery);
+
+
+$(".send").on('click', function(){
+	$(".text").addClass("active");
+	$(".send").addClass("active");
+	$(".loader").addClass("active");
+	$(".send").delay(1700).queue(function(){
+        $(this).addClass("finished").clearQueue();
+    });
+	
+	$(".done").delay(1600).queue(function(){
+        $(this).addClass("active").clearQueue();
+    });
+})
